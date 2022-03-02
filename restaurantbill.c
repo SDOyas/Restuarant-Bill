326c56970f29252a3b75ff2b2b3f2caf018c020d
Simon Oyas <sdoyas@cpp.edu>
2022-03-01 16:17:42 -0800

    First Step

--- restaurantbill.c
+++ restaurantbill.c
@@ -3,3 +3,7 @@
 //third make interface where user can input cost, tax, and tip
 //fourth create "Bill" that shows all inputs
 //fifth calculation for total and print total
+#include <stdio.h>
+int main(){
+    
+}
