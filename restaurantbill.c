b1303eecfd523763230fd5466946ffc8cb89eddb
Simon Oyas <sdoyas@cpp.edu>
2022-03-01 16:15:40 -0800

    Created Outline

---
+++ restaurantbill.c
@@ -0,0 +1,5 @@
+//first include standard io
+//second declare variables
+//third make interface where user can input cost, tax, and tip
+//fourth create "Bill" that shows all inputs
+//fifth calculation for total and print total
